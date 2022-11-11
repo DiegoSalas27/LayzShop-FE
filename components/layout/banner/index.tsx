eeb6c5b0091a6a64ba3869a16351ceb6da7bb8ac
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-11-11 09:14:32 -0600

    feat: added dummy about page

---
+++ components/layout/banner/index.tsx
@@ -0,0 +1,30 @@
+function Banner({ title }: any) {
+  return (
+    <div
+      className="carousel relative container mx-auto"
+      style={{ maxWidth: 1600 }}
+    >
+      <div className="carousel-inner relative overflow-hidden w-full">
+        <div
+          className="carousel-item absolute"
+          style={{ height: "25vh", position: "static", opacity: 100 }}
+        >
+          <div
+            className="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right"
+            style={{
+              backgroundImage: `url('https://images.unsplash.com/photo-1422190441165-ec2956dc9ecc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80')`,
+            }}
+          >
+            <div className="container mx-auto">
+              <div className="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
+                <p className="text-black text-2xl my-4">{title}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,72 @@
-import styles from '../styles/Home.module.css'
+import { FC } from "react";
+import Banner from "../components/layout/banner";
+import Layout from "../components/layout/main-layout/main-layout";
+import PageWithLayoutType from "../components/layout/types/page-with-layout-type";
 
 function About() {
-  return(
-    <div className={styles.container}>
-      <h1>The About Page</h1>
-    </div>
-  )
+  return (
+    <section className="bg-white py-8">
+      <Banner title="About LazyShop" />
+      <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12 px-20">
+        <h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            excepturi vitae fugiat velit provident officiis nihil rem quo!
+            Assumenda ut quae perferendis ex unde expedita nobis modi ipsam
+            totam dolorem?
+          </p>
+          <p>
+            Nihil accusamus eligendi soluta distinctio obcaecati odit
+            repudiandae iusto perferendis rem saepe fugiat corrupti
+            exercitationem ut hic, facilis velit voluptate unde, quam officia
+            molestiae! Repellat odio dolores in reprehenderit? Dolor?
+          </p>
+          <p>
+            Cum, nobis nulla consequuntur architecto debitis itaque deserunt
+            exercitationem similique repellendus, inventore quod facere delectus
+            perspiciatis, eos suscipit quisquam. In dicta obcaecati laborum ut
+            nulla id aut itaque, eligendi esse?
+          </p>
+          <p>
+            Magni autem vitae eveniet rerum magnam voluptatem minus quas
+            perferendis veritatis tempore, ipsum officiis praesentium, iure
+            molestiae amet vel rem eius velit, odit dolor ducimus molestias!
+            Omnis dicta amet quam?
+          </p>
+        </h1>
+        <br/>
+        <h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            excepturi vitae fugiat velit provident officiis nihil rem quo!
+            Assumenda ut quae perferendis ex unde expedita nobis modi ipsam
+            totam dolorem?
+          </p>
+          <p>
+            Nihil accusamus eligendi soluta distinctio obcaecati odit
+            repudiandae iusto perferendis rem saepe fugiat corrupti
+            exercitationem ut hic, facilis velit voluptate unde, quam officia
+            molestiae! Repellat odio dolores in reprehenderit? Dolor?
+          </p>
+          <p>
+            Cum, nobis nulla consequuntur architecto debitis itaque deserunt
+            exercitationem similique repellendus, inventore quod facere delectus
+            perspiciatis, eos suscipit quisquam. In dicta obcaecati laborum ut
+            nulla id aut itaque, eligendi esse?
+          </p>
+          <p>
+            Magni autem vitae eveniet rerum magnam voluptatem minus quas
+            perferendis veritatis tempore, ipsum officiis praesentium, iure
+            molestiae amet vel rem eius velit, odit dolor ducimus molestias!
+            Omnis dicta amet quam?
+          </p>
+        </h1>
+      </div>
+    </section>
+  );
 }
 
-export default About;
+const about: FC = () => <About />;
+(about as PageWithLayoutType).layout = Layout;
+
+export default about;
